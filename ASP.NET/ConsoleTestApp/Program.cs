3ecb2dadf3f32517b86cd4d6d991878affdda6ec
Werner, Christian <christian.werner.biz@gmail.com>
2025-03-25 21:38:36 +0100

    CW-25-03-25: Added BinarySearch logic, Added Project 'Misc'

--- ASP.NET/ConsoleTestApp/Program.cs
+++ ASP.NET/ConsoleTestApp/Program.cs
@@ -1,5 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Test: Deutscher Text
+// Test: English Text
+
+int[] ints = { -9, 14, 37, 102 };
+
+var testVarible = 1;
+
+Console.WriteLine(Answer.Exists(ints, 102)); // true
+Console.WriteLine(Answer.Exists(ints, 36)); // false
+
+public class Answer
+{
+    public static bool Exists(int[] ints, int k)
+    {
+        // Binäre Suche verwenden, da das Array sortiert ist (O(log n) Laufzeit)
+        return Array.BinarySearch(ints, k) >= 0;
+
+        int leftPos = 0;
+        int rightPos = ints.Length - 1;
+
+        while(leftPos <= rightPos)
+        {
+            int midPos = leftPos + (rightPos - leftPos) / 2;
+            if (ints[midPos] == k)
+            {
+                return true;
+            }
+            else if (ints[midPos] < k)
+            {
+                leftPos = midPos + 1;
+            }
+            else
+            {
+                rightPos = midPos - 1;
+            }
+        }
+
+        return false;
+    }
+}
+
+
 
 
 
